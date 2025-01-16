b318ae5e79f05c67814fa64f8f6c48d9eb339a1d
Mury05 <murielleodilone@gmail.com>
2025-01-16 15:51:34 +0100

    Add one-to-many relationships for Category, Editor, and Shelf models

--- src/main/java/bj/highfive/book/model/Category.java
+++ src/main/java/bj/highfive/book/model/Category.java
@@ -1,10 +1,13 @@
 package bj.highfive.book.model;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,4 +20,7 @@ public class Category {
     private Long id;
     @Column(nullable = false)
     private String nom;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Book> books;
 }
--- src/main/java/bj/highfive/book/model/Editor.java
+++ src/main/java/bj/highfive/book/model/Editor.java
@@ -1,10 +1,13 @@
 package bj.highfive.book.model;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -21,4 +24,6 @@ public class Editor {
     private String email;
     private String adresse;
 
+    @OneToMany(mappedBy = "editor")
+    private Set<Book> books;
 }
