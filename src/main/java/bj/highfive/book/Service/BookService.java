d371b01802ee3eeb98355131ac3dfa212c4bbe55
Mury05 <murielleodilone@gmail.com>
2025-01-17 10:09:39 +0100

    Ajout du service BookService et mise à jour du contrôleur BookController pour la gestion des livres

---
+++ src/main/java/bj/highfive/book/Service/BookService.java
@@ -0,0 +1,54 @@
+package bj.highfive.book.Service;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bj.highfive.book.model.Author;
+import bj.highfive.book.model.Book;
+import bj.highfive.book.repository.BookRepository;
+import org.springframework.beans.BeanUtils;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Ajouter
+    public Book newBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBook(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Book updateBook(Long id, Book updatedBook) {
+        Book existingBook = bookRepository.findById(id).orElse(null);
+        if (existingBook != null) {
+            // Copier les propriétés du livre mis à jour dans le livre existant
+            BeanUtils.copyProperties(updatedBook, existingBook, "id");
+            return bookRepository.save(existingBook);
+        }
+        return null;
+    }
+    // public List<Book> searchBooks(Author author, LocalDate year) {
+    //     if (year != null) {
+    //         return bookRepository.findByAuthorAndYear(author, year);
+    //     } else {
+    //         return bookRepository.findByAuthor(author);
+    //     }
+    // }
+
+}
--- src/main/java/bj/highfive/book/controller/BookController.java
+++ src/main/java/bj/highfive/book/controller/BookController.java
@@ -3,50 +3,75 @@ package bj.highfive.book.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import bj.highfive.book.Service.BookService;
+import bj.highfive.book.model.Book;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
     // private String allBooks = {};
     List<String> allBooks = new ArrayList<>();
 
     @GetMapping("")
-    public String getAllBooks() {
-        return "Liste de tous les livres";
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
     }
 
     @PostMapping("")
-    public String addBook(){
-        return "Livre ajouté avec succès !";
+    public String addBook(@RequestBody Book book){
+        bookService.newBook(book);
+        return "Livre enregistré avec succès !";
     }
 
     @GetMapping("/{id}")
-    public String getBookById(@PathVariable("id") String bookId){
-        return "Le livre avec l'id " + bookId + " est : Les Misérables";
+    public Book getBookById(@PathVariable("id") Long bookId){
+        
+        return bookService.getBook(bookId);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteBook(@PathVariable("id") String bookId){
-        return "Le livre à supprimer a l'id " + bookId;
+    public String deleteBook(@PathVariable("id") Long bookId){
+        bookService.deleteBook(bookId);
+        return "Le livre avec l'id " + bookId + " supprimé avec succès !";
     }
 
+    // @PutMapping("/{id}")
+    // public String updateBook(@PathVariable("id") String bookId){
+    //     return "Le livre à modifier a l'id " + bookId;
+    // }
+
     @PutMapping("/{id}")
-    public String updateeBook(@PathVariable("id") String bookId){
-        return "Le livre à modifier a l'id " + bookId;
+public String updateBook(@PathVariable("id") Long bookId, @RequestBody Book updatedBook) {
+    Book updated = bookService.updateBook(bookId, updatedBook);
+    if (updated != null) {
+        return "Livre mis à jour avec succès !";
     }
+    return "Livre introuvable avec l'id " + bookId;
+}
+
 
     @GetMapping("/search")
     public String searchBooks(@RequestParam String author, @RequestParam(required = false) Long year){
         return "Recherche des livres de l'author " + author + " de l'année " + year;
     }
+//     @GetMapping("/search")
+// public List<Book> searchBooks(@RequestParam String author, @RequestParam(required = false) Long year) {
+//     return bookService.searchBooks(author, year);
+// }
+
 
 }
